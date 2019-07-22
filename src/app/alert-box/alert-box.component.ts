8532934f79a6a199356eeb74012216c3b0170629
G Pranesh <pranesh.g1@sc.com>
2019-07-22 23:26:57 +0530

    Add cancel button to alert box

--- src/app/alert-box/alert-box.component.ts
+++ src/app/alert-box/alert-box.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Inject } from '@angular/core';
 import {
   MAT_DIALOG_DATA
 } from "@angular/material";
+import { NGXLogger } from 'ngx-logger';
+import { MatDialogRef } from '@angular/material';
 
 @Component({
   selector: 'app-alert-box',
@@ -10,9 +12,13 @@ import {
 })
 export class AlertBoxComponent implements OnInit {
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data:any) { }
+  constructor(@Inject(MAT_DIALOG_DATA) public data:any, private logger: NGXLogger,private dialogRef: MatDialogRef<AlertBoxComponent>) { }
 
   ngOnInit() {
+  }
+
+  cancel() {
+    this.dialogRef.close({isCancel: true});
   }
 
 }
