6e728947bb9002e14e6ce3d47f46e35dcc458de2
G Pranesh <pranesh.g1@sc.com>
2019-07-22 23:37:22 +0530

    Add file type filter, search and multiple download features

--- src/app/listing/listing.component.ts
+++ src/app/listing/listing.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer, ElementRef } from '@angular/core';
 import { HttpService } from '../http.service';
 import { MatDialog, MatDialogConfig } from '@angular/material';
 import { Router } from '@angular/router';
@@ -6,6 +6,9 @@ import FilesList from '../FilesList';
 import { AlertBoxComponent } from '../alert-box/alert-box.component';
 import { NGXLogger } from 'ngx-logger';
 import { UtilityService } from '../utility.service';
+import * as _ from 'underscore';
+import FileInfo from '../File';
+import { Socket } from 'ngx-socket-io';
 
 @Component({
   selector: 'app-listing',
@@ -14,40 +17,108 @@ import { UtilityService } from '../utility.service';
 })
 export class ListingComponent implements OnInit {
 
-  filesHostPort = '3013';
+  filesPort = '3013';
+
+  searchInput: string;
+
+  allFiles: FilesList = { list: [], ip: '' };
 
   files: FilesList = { list: [], ip: '' };
 
-  selectedFiles: Array<String> = [];
+  fileTypes: Array<String> = [];
+
+  selectedFiles: Array<FileInfo> = [];
 
   hasFilesSelected: Boolean = false;
 
-  constructor(private http: HttpService, private router: Router, private dialog: MatDialog, private logger: NGXLogger, private utility: UtilityService) {
+  currentFileType: String;
+
+  sortOrder: String;
+
+  constructor(private http: HttpService, private router: Router, private dialog: MatDialog, private logger: NGXLogger, private utility: UtilityService, private socket: Socket, private renderer: Renderer, private elem: ElementRef) {
     this.utility.setTitle('View files');
   }
 
   ngOnInit() {
     this.getFiles();
+    var that = this;
+    var socket = this.socket.connect();
+    socket.on('change', function(data:FilesList){
+      that.updateFilesList(data);
+    });
+  }
+
+  updateFilesList(data:FilesList) {
+    // Redirect to upload file page when no files to list
+    if (!data.list) {
+      this.logger.log('Directory is empty');
+      this.navigateToHome();
+    }
+    this.resetSelected();
+    this.resetFileInfo();
+
+    this.allFiles = {...data};
+    this.files.ip = this.allFiles.ip;
+    // Sort files descending
+    this.sortFilesByTime(data, 'desc'); 
+    // Get file types 
+    this.getFileTypes();
+  }
+
+  resetSelected(){
+    _.map(this.selectedFiles,(file)=>{
+       file.checked = false;
+    });
+    this.selectedFiles = [];
+    this.hasFilesSelected = false;
+  }
+
+  resetFileInfo(){
+    this.currentFileType = '';
+    this.searchInput = '';
   }
 
   getFiles() {
     this.http.getFiles().subscribe((data: FilesList) => {
-      this.files = data;
-      if (!data.list.length) {
-        this.navigateToHome();
-      }
+      this.updateFilesList(data);
     });
   }
+  
+  sortFilesByTime (files: FilesList, sortOrder: String){
+    let list = _.sortBy(files.list,function (file){
+      return  file.modifiedTime;
+    });
+    if(sortOrder == 'desc'){
+      list = list.reverse();
+    }
+    this.sortOrder = sortOrder;
+    this.files.list = list;
+  }
 
-  selectFile(selectedFile: String) {
+  getFileTypes(){
+    let fileTypes = [];
+    let list = this.files.list;
+    fileTypes = list.map((file) => {
+        let splitName = file.name.split('.'),
+            fileType: String;
+        fileType = splitName.splice(-1)[0];
+        return fileType;
+    });
+    fileTypes = _.uniq(fileTypes);
+    this.fileTypes = fileTypes;
+  }
+
+
+  selectFile(selectedFile: FileInfo,$event) {
     let selectedFiles = this.selectedFiles;
     let filteredFiles = [];
+    selectedFile.checked = $event.checked;
     if (selectedFiles.length == 0) {
       selectedFiles.push(selectedFile);
     } else {
       // Remove already selected file if it has been chosen
       filteredFiles = selectedFiles.reduce(function (result, file) {
-        if (file != selectedFile) {
+        if (file.name != selectedFile.name) {
           result.push(file);
         }
         return result;
@@ -67,7 +138,7 @@ export class ListingComponent implements OnInit {
     let that = this;
     const config = new MatDialogConfig();
     config.data = {
-      message: 'No files to display'
+      message: this.utility.getMessageByType('noFiles')
     };
     let dialogRef = this.dialog.open(AlertBoxComponent, config);
     dialogRef.afterClosed().subscribe(result => {
@@ -78,29 +149,111 @@ export class ListingComponent implements OnInit {
   deleteFile(fileName) {
     this.http.deleteSingle(fileName).subscribe(res => {
       this.logger.log('File deleted successfully');
-      this.getFiles();
+      // this.getFiles();
     });
   }
 
   deleteAllFiles() {
-    this.http.deleteAll().subscribe(res => {
-      this.logger.log('All files deleted successfully');
-      this.files = { list: [], ip: '' };
-      this.navigateToHome();
+    const config = new MatDialogConfig();
+    config.data = {
+      message: this.utility.getMessageByType('delAllFiles'),
+      hasCancelBtn: true
+    };
+    let dialogRef = this.dialog.open(AlertBoxComponent, config);
+    dialogRef.afterClosed().subscribe(res => {
+      if ((res && !res.isCancel) || !res) {
+        this.http.deleteAll().subscribe(res => {
+          this.logger.log('All files deleted successfully');
+          // this.files = { list: [], ip: '' };
+          // this.navigateToHome();
+        });
+      }
     });
+
   }
 
   deleteSelectedFiles() {
-    this.http.deleteSelected(this.selectedFiles).subscribe(res => {
-      this.logger.log('Selected files deleted successfully');
-      this.selectedFiles = [];
-      this.getFiles();
+    const config = new MatDialogConfig();
+    config.data = {
+      message: this.utility.getMessageByType('delSelectedFiles'),
+      hasCancelBtn: true
+    };
+    let dialogRef = this.dialog.open(AlertBoxComponent, config);
+    dialogRef.afterClosed().subscribe(res => {
+      if ((res && !res.isCancel) || !res) {
+        this.http.deleteSelected(this.selectedFiles).subscribe(res => {
+          this.logger.log('Selected files deleted successfully');
+          this.selectedFiles = [];
+        });
+      }
     });
   }
 
-  getFileLink(fileName) {
+  openFile(fileName) {
     let host = this.files.ip,
-      url = `http://${host}:${this.filesHostPort}/${fileName}`;
+      url = `http://${host}:${this.filesPort}/${fileName}`;
     window.open(url, '_blank');
+  }
+
+  filterFilesByType(type:String){
+    let filteredFiles,files;
+    if(this.searchInput){
+      files = this.files.list;
+    } else {
+      files = this.allFiles.list;
+    }
+    
+    if(type != this.currentFileType){
+      filteredFiles = files.filter((file) => {
+        let fileType = file.name.split('.').splice(-1)[0];
+        if(fileType == type){
+          return file;
+        }
+      });
+      this.currentFileType = type;
+    } else {
+      filteredFiles = {...this.allFiles}.list;
+      this.currentFileType = '';
+    }
+    this.files.list = filteredFiles;
+    this.sortFilesByTime({...this.files},'desc');
+    this.resetSelected();
+  }
+
+  searchFiles(){
+    if(this.searchInput) {
+      let that = this,filteredList = [];
+      _.each(this.allFiles.list,(file)=>{
+        let name = file.name.toLowerCase(),
+            searchInput = that.searchInput.toLowerCase();
+        if(name.indexOf(searchInput) > -1) {
+          filteredList.push(file);
+        }
+      });
+      this.files.list = filteredList;
+      this.sortFilesByTime({...this.files},'desc');
+      this.getFileTypes();
+    } else {
+      this.clearSearch();
+    }
+  }
+
+  clearSearch(){
+    this.searchInput = '';
+    this.currentFileType = '';
+    this.sortFilesByTime({...this.allFiles}, 'desc');
+    this.getFileTypes();
+  }
+
+  refreshList(){
+    this.getFiles();
+  }
+
+  downloadSelectedFiles(){
+    let selectedFiles = {...this.selectedFiles};
+    this.resetSelected();
+    _.map(selectedFiles,(file:FileInfo)=>{
+        this.openFile(file.name);
+    });
   }
 }
