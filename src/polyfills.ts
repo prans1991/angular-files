dbe4380effc0185809e08c6c6c43cd195282d1cb
G Pranesh <pranesh.g1@sc.com>
2019-07-22 23:41:47 +0530

    Add global as window to fix socket io issue with Angular 7

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -32,3 +32,4 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+(window as any).global = window;
