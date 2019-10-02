28c696407b3ee93d39450f2220f5087db0673747
G Pranesh <pranesh.g1@sc.com>
2019-10-02 20:38:39 +0530

    Add alert messages JSON

--- src/app/utility.service.ts
+++ src/app/utility.service.ts
@@ -1,22 +1,11 @@
 import { Injectable } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import alertMessages from '../assets/alert-messages.json';
 
 @Injectable()
 export class UtilityService {
 
   host: String;
-
-  messages = {
-    'delAllFiles': 'Are you sure you want to delete all files?',
-    'delSelectedFiles': 'Are you sure you want to delete the selected files?',
-    'noFiles': 'No files to display',
-    'noFilesUploadSome': 'No files to display. Please upload some.',
-    'selectFiles': 'Please select files to upload',
-    'maxSelect': 'Please select a maximum of 5 files',
-    'duplicateSelect': 'Duplicate files selected for upload. Please check and retry upload',
-    'uploadSuccess': 'File(s) uploaded successfully',
-    'uploadingFiles': 'File(s) are being uploaded. Please wait ...'
-  };
 
   constructor(private title: Title) {
     this.host = new URL(window.location.href).hostname;
@@ -31,6 +20,6 @@ export class UtilityService {
   }
 
   getMessageByType(type: string): Object{
-    return this.messages[type];
+    return alertMessages[type];
   }
 }
