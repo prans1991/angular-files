07612b95c33b486d1c0a2d0db48d2df34b54f46d
G Pranesh <pranesh.g1@sc.com>
2020-01-25 16:41:25 +0530

    Add scroll to top arrow when there is a scroll

--- src/app/listing/listing.component.ts
+++ src/app/listing/listing.component.ts
@@ -11,21 +11,25 @@ import * as _ from 'underscore';
 import FileInfo from '../File';
 import * as moment from 'moment';
 import { PreviewModalComponent } from '../preview-modal/preview-modal.component';
+import { interval, Subscription } from "rxjs";
+
 
 @Component({
-  selector: 'app-listing',
-  templateUrl: './listing.component.html',
-  styleUrls: ['./listing.component.scss']
+  selector: "app-listing",
+  templateUrl: "./listing.component.html",
+  styleUrls: ["./listing.component.scss"]
 })
 export class ListingComponent implements OnInit {
 
-  filesPort = '3013';
+  subscription: Subscription;
+
+  filesPort = "3013";
 
   searchInput: string;
 
-  allFiles: FilesList = { list: [], ip: '' };
+  allFiles: FilesList = { list: [], ip: "" };
 
-  files: FilesList = { list: [], ip: '' };
+  files: FilesList = { list: [], ip: "" };
 
   fileTypes: Array<String> = [];
 
@@ -43,40 +47,63 @@ export class ListingComponent implements OnInit {
 
   isAdmin: Boolean = false;
 
-  previewFileTypes: Array<String> = ['png', 'gif', 'jpg', 'jpeg'];
+  previewFileTypes: Array<String> = ["png", "gif", "jpg", "jpeg"];
 
   hasFetchedList: Boolean = false;
 
-  loadingImgPath: String = 'assets/loading.gif';
+  loadingImgPath: String = "assets/loading.gif";
 
   isFetchingList: Boolean = false;
 
-  constructor(private http: HttpService, private router: Router, private dialog: MatDialog, private logger: NGXLogger, private utility: UtilityService, private route: ActivatedRoute, private location: Location) {
-    this.utility.setTitle('Share Files');
+  showScrollTop: Boolean = false;
+
+  constructor(
+    private http: HttpService,
+    private router: Router,
+    private dialog: MatDialog,
+    private logger: NGXLogger,
+    private utility: UtilityService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {
+    this.utility.setTitle("Share Files");
   }
 
   ngOnInit() {
     this.route.queryParams.subscribe((params: any) => {
       this.qParams = params;
-      this.isAdmin = params.hasOwnProperty('admin');
-      this.location.replaceState('');
+      this.isAdmin = params.hasOwnProperty("admin");
+      this.location.replaceState("");
     });
     this.getFiles();
     var that = this;
-    this.http.socket.on('change', function (data: FilesList) {
+    this.http.socket.on("change", function(data: FilesList) {
       that.updateFilesList(data);
     });
 
-    this.http.socket.on('connections', function (data: any) {
+    this.http.socket.on("connections", function(data: any) {
       that.connections = data;
     });
+
+    const source = interval(3000);
+    this.subscription = source.subscribe(val =>
+      this.checkScrollY()
+    );
   }
+
+  ngOnDestroy() {
+    this.subscription && this.subscription.unsubscribe();
+  }
+
+  checkScrollY = (): void => {
+    this.showScrollTop = window.scrollY > 100;
+  };
 
   updateFilesList(data: FilesList) {
     this.hasFetchedList = true;
     // Redirect to upload file page when no files to list
     if (!data.list) {
-      this.logger.log('Directory is empty');
+      this.logger.log("Directory is empty");
       this.navigateToHome();
     }
     this.resetSelected();
@@ -85,13 +112,13 @@ export class ListingComponent implements OnInit {
     this.allFiles = { ...data };
     this.files.ip = this.allFiles.ip;
     // Sort files descending
-    this.sortFilesByTime(data, 'desc');
-    // Get file types 
+    this.sortFilesByTime(data, "desc");
+    // Get file types
     this.getFileTypes();
   }
 
   resetSelected() {
-    _.map(this.selectedFiles, (file) => {
+    _.map(this.selectedFiles, file => {
       file.checked = false;
     });
     this.selectedFiles = [];
@@ -99,23 +126,23 @@ export class ListingComponent implements OnInit {
   }
 
   resetFileInfo() {
-    this.currentFileType = '';
-    this.searchInput = '';
+    this.currentFileType = "";
+    this.searchInput = "";
   }
 
   getFiles() {
     this.isFetchingList = true;
     this.http.getFiles().subscribe((data: FilesList) => {
-        this.isFetchingList = false;
-        this.updateFilesList(data);
+      this.isFetchingList = false;
+      this.updateFilesList(data);
     });
   }
 
   sortFilesByTime(files: FilesList, sortOrder: String) {
-    let list = _.sortBy(files.list, function (file) {
+    let list = _.sortBy(files.list, function(file) {
       return file.modifiedTime;
     });
-    if (sortOrder == 'desc') {
+    if (sortOrder == "desc") {
       list = list.reverse();
     }
     this.sortOrder = sortOrder;
@@ -125,8 +152,8 @@ export class ListingComponent implements OnInit {
   getFileTypes() {
     let fileTypes = [];
     let list = this.files.list;
-    fileTypes = list.map((file) => {
-      let splitName = file.name.split('.'),
+    fileTypes = list.map(file => {
+      let splitName = file.name.split("."),
         fileType: String;
       fileType = splitName.splice(-1)[0].toLowerCase();
       return fileType;
@@ -134,7 +161,6 @@ export class ListingComponent implements OnInit {
     fileTypes = _.uniq(fileTypes);
     this.fileTypes = fileTypes;
   }
-
 
   selectFile(selectedFile: FileInfo, $event) {
     let selectedFiles = this.selectedFiles;
@@ -144,13 +170,13 @@ export class ListingComponent implements OnInit {
       selectedFiles.push(selectedFile);
     } else {
       // Remove already selected file if it has been chosen
-      filteredFiles = selectedFiles.reduce(function (result, file) {
+      filteredFiles = selectedFiles.reduce(function(result, file) {
         if (file.name != selectedFile.name) {
           result.push(file);
         }
         return result;
       }, []);
-      // If the file selected is new add to selected files 
+      // If the file selected is new add to selected files
       if (filteredFiles.length == selectedFiles.length) {
         selectedFiles.push(selectedFile);
       } else {
@@ -158,56 +184,55 @@ export class ListingComponent implements OnInit {
       }
     }
     this.selectedFiles = selectedFiles;
-    this.hasFilesSelected = (this.selectedFiles.length > 0);
+    this.hasFilesSelected = this.selectedFiles.length > 0;
   }
 
   navigateToHome() {
     let that = this;
     const config = new MatDialogConfig();
     config.data = {
-      message: this.utility.getMessageByType('noFiles')
+      message: this.utility.getMessageByType("noFiles")
     };
     let dialogRef = this.dialog.open(AlertBoxComponent, config);
     dialogRef.afterClosed().subscribe(result => {
-      that.router.navigate(['']);
+      that.router.navigate([""]);
     });
   }
 
   deleteFile(fileName, $event) {
     $event.stopPropagation();
     this.http.deleteSingle(fileName).subscribe(res => {
-      this.logger.log('File deleted successfully');
+      this.logger.log("File deleted successfully");
     });
   }
 
   deleteAllFiles() {
     const config = new MatDialogConfig();
     config.data = {
-      message: this.utility.getMessageByType('delAllFiles'),
+      message: this.utility.getMessageByType("delAllFiles"),
       hasCancelBtn: true
     };
     let dialogRef = this.dialog.open(AlertBoxComponent, config);
     dialogRef.afterClosed().subscribe(res => {
       if ((res && !res.isCancel) || !res) {
         this.http.deleteAll().subscribe(res => {
-          this.logger.log('All files deleted successfully');
+          this.logger.log("All files deleted successfully");
         });
       }
     });
-
   }
 
   deleteSelectedFiles() {
     const config = new MatDialogConfig();
     config.data = {
-      message: this.utility.getMessageByType('delSelectedFiles'),
+      message: this.utility.getMessageByType("delSelectedFiles"),
       hasCancelBtn: true
     };
     let dialogRef = this.dialog.open(AlertBoxComponent, config);
     dialogRef.afterClosed().subscribe(res => {
       if ((res && !res.isCancel) || !res) {
         this.http.deleteSelected(this.selectedFiles).subscribe(res => {
-          this.logger.log('Selected files deleted successfully');
+          this.logger.log("Selected files deleted successfully");
           this.selectedFiles = [];
         });
       }
@@ -215,13 +240,17 @@ export class ListingComponent implements OnInit {
   }
 
   openFile(fileName) {
-    let fileType = fileName.split('.').slice(-1).pop().toLowerCase();
+    let fileType = fileName
+      .split(".")
+      .slice(-1)
+      .pop()
+      .toLowerCase();
     if (_.contains(this.previewFileTypes, fileType)) {
-      let isPdf = (fileType == 'pdf');
+      let isPdf = fileType == "pdf";
       this.openPreviewModal(fileName, isPdf);
     } else {
       let url = `http://${this.utility.host}:${this.filesPort}/${fileName}`;
-      window.open(url, '_blank');
+      window.open(url, "_blank");
     }
   }
 
@@ -233,23 +262,29 @@ export class ListingComponent implements OnInit {
       files = this.allFiles.list;
     }
 
-    filteredFiles = files.filter((file) => {
-      let fileType = file.name.split('.').splice(-1)[0];
-      if (fileType == type) {
-        return file;
-      }
-    });
+    if (type) {
+      filteredFiles = files.filter(file => {
+        let fileType = file.name.split(".").splice(-1)[0];
+        if (fileType == type) {
+          return file;
+        }
+      });
+    } else {
+      filteredFiles = files;
+    }
+
     this.currentFileType = type;
 
     this.files.list = filteredFiles;
-    this.sortFilesByTime({ ...this.files }, 'desc');
+    this.sortFilesByTime({ ...this.files }, "desc");
     this.resetSelected();
   }
 
   searchFiles() {
     if (this.searchInput) {
-      let that = this, filteredList = [];
-      _.each(this.allFiles.list, (file) => {
+      let that = this,
+        filteredList = [];
+      _.each(this.allFiles.list, file => {
         let name = file.name.toLowerCase(),
           searchInput = that.searchInput.toLowerCase();
 
@@ -258,7 +293,7 @@ export class ListingComponent implements OnInit {
         }
       });
       this.files.list = filteredList;
-      this.sortFilesByTime({ ...this.files }, 'desc');
+      this.sortFilesByTime({ ...this.files }, "desc");
       this.getFileTypes();
     } else {
       this.clearSearch();
@@ -266,9 +301,9 @@ export class ListingComponent implements OnInit {
   }
 
   clearSearch() {
-    this.searchInput = '';
-    this.currentFileType = '';
-    this.sortFilesByTime({ ...this.allFiles }, 'desc');
+    this.searchInput = "";
+    this.currentFileType = "";
+    this.sortFilesByTime({ ...this.allFiles }, "desc");
     this.getFileTypes();
   }
 
@@ -287,7 +322,7 @@ export class ListingComponent implements OnInit {
   isUploadedRecent(uploadedDate: any) {
     let currentTime = moment(moment.now());
     uploadedDate = new Date(uploadedDate);
-    let diff = currentTime.diff(uploadedDate, 'days');
+    let diff = currentTime.diff(uploadedDate, "days");
     return diff <= 7;
   }
 
@@ -299,5 +334,17 @@ export class ListingComponent implements OnInit {
       isPdf: isPdf
     };
     this.dialog.open(PreviewModalComponent, config);
+  }
+
+  scrollToTop() {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: "smooth"
+    });
+  }
+
+  resetFileType() {
+    this.updateFilesList(this.allFiles);
   }
 }
