4278962999063be95b035c93b1b7e93af5ae95bd
G Pranesh <pranesh.g1@sc.com>
2020-04-19 19:00:34 +0530

    Add progress bar and display animation for upload progress

--- src/app/alert-box/alert-box.component.ts
+++ src/app/alert-box/alert-box.component.ts
@@ -6,24 +6,28 @@ import { NGXLogger } from 'ngx-logger';
 import { MatDialogRef } from '@angular/material';
 
 @Component({
-  selector: 'app-alert-box',
-  templateUrl: './alert-box.component.html',
-  styleUrls: ['./alert-box.component.scss']
+  selector: "app-alert-box",
+  templateUrl: "./alert-box.component.html",
+  styleUrls: ["./alert-box.component.scss"]
 })
 export class AlertBoxComponent implements OnInit {
-
   hasDialogAction: Boolean = true;
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private logger: NGXLogger, private dialogRef: MatDialogRef<AlertBoxComponent>) {
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private logger: NGXLogger,
+    private dialogRef: MatDialogRef<AlertBoxComponent>
+  ) {
     dialogRef.disableClose = true;
-    this.hasDialogAction = (data.hasDialogAction != undefined) ? data.hasDialogAction : this.hasDialogAction;
+    this.hasDialogAction =
+      data.hasDialogAction != undefined
+        ? data.hasDialogAction
+        : this.hasDialogAction;
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   cancel() {
     this.dialogRef.close({ isCancel: true });
   }
-
 }
